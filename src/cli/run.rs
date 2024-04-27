dd47b88c2ac1987c1a9162c4301dedfda2b4114b
Brian Bruggeman <1350114+brianbruggeman@users.noreply.github.com>
2024-04-26 19:49:18 -0500

    Fixes for updates

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -9,6 +9,6 @@ pub async fn run() -> anyhow::Result<()> {
     let repos_to_clone = core::compare_repos(&remote_repos, &local_repos);
     core::clone_repos(&repos_to_clone, &command.path, &command.github_token).await?;
     let combined_repos = core::combine_repos(&local_repos, &repos_to_clone);
-    core::update_repos(&combined_repos).await?;
+    core::update_repos(&combined_repos, &command.path, &command.github_token).await?;
     Ok(())
 }
--- src/core/git.rs
+++ src/core/git.rs
@@ -48,7 +48,20 @@ pub async fn clone_repo(
     github_token: &str,
 ) -> anyhow::Result<()> {
     // git clone <url>
-    git(&["clone", &repo.ssh_url, &repo.name], path, github_token).await
+    git(&["clone", &repo.ssh_url, &repo.name], path, github_token).await?;
+    Ok(())
+}
+
+pub async fn is_dirty(repo_path: impl AsRef<Path>) -> anyhow::Result<bool> {
+    // git status --porcelain
+    let output = git(&["status", "--porcelain"], repo_path, "").await?;
+    Ok(!output.is_empty())
+}
+
+pub async fn is_main_branch(repo_path: impl AsRef<Path>) -> anyhow::Result<bool> {
+    // git rev-parse --abbrev-ref HEAD
+    let output = git(&["rev-parse", "--abbrev-ref", "HEAD"], repo_path, "").await?;
+    Ok(output.trim() == "main")
 }
 
 // Executes a git command with Command
@@ -56,20 +69,17 @@ pub async fn git(
     args: &[impl AsRef<str>],
     path: impl AsRef<Path>,
     github_token: impl AsRef<str>,
-) -> anyhow::Result<()> {
+) -> anyhow::Result<String> {
     // Setup command to execute git command
     let args = args.iter().map(|arg| arg.as_ref()).collect::<Vec<_>>();
     let output = Command::new("git")
         .args(&args)
         .current_dir(path)
-        // .env("GIT_TERMINAL_PROMPT", "0") // Disable terminal prompt for credentials
-        // .env("GIT_ASKPASS", "echo") // Use echo to avoid providing a password
-        // .env("GIT_SSH_COMMAND", format!("ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no").as_str())
         .env("GITHUB_TOKEN", github_token.as_ref())
         .output()?;
 
     if output.status.success() {
-        Ok(())
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
     } else {
         Err(anyhow::anyhow!(
             "Failed to execute git command: `git {}` - {}",
@@ -81,25 +91,55 @@ pub async fn git(
 
 pub async fn fetch_repo(repo_path: impl AsRef<Path>, github_token: &str) -> anyhow::Result<()> {
     // git fetch --all
-    git(&["fetch", "--all"], repo_path, github_token).await
+    git(&["fetch", "--all"], repo_path, github_token).await?;
+    Ok(())
 }
 
 pub async fn pull_repo(repo_path: impl AsRef<Path>, github_token: &str) -> anyhow::Result<()> {
     // git pull
-    git(&["pull"], repo_path, github_token).await
-}
-
-pub async fn update_repos(repos: &[Repo]) -> anyhow::Result<()> {
-    for repo in repos {
-        let path = Path::new(&repo.name);
-        if !path.exists() {
-            let url = &repo.ssh_url;
-            let _output = Command::new("git")
-                .arg("clone")
-                .arg(url)
-                .arg(&repo.name)
-                .output()?;
-        }
-    }
+    git(&["pull"], repo_path, github_token).await?;
     Ok(())
 }
+
+pub async fn update_repos(repos: &[Repo], path: impl AsRef<Path>, github_token: &str) -> anyhow::Result<()> {
+    let path = path.as_ref();
+    if !path.exists() {
+        fs::create_dir_all(path)?;
+    }
+
+    let futures = repos.iter().map(|repo| {
+        let repo_path = path.join(&repo.name);
+        let token_clone = github_token.to_owned();
+
+        // Spawn a new asynchronous task for each repository
+        task::spawn(async move {
+            let token = token_clone.clone();
+
+            // Fetch the repository
+            fetch_repo(&repo_path, &token).await?;
+
+            // Check if the repository is on `main` branch and not dirty
+            if is_main_branch(&repo_path).await? && !is_dirty(&repo_path).await? {
+                // Pull the repository
+                pull_repo(&repo_path, &token).await?;
+            }
+
+            Ok(())
+        })
+    });
+
+    // Collect all futures into a Vec
+    let futures_collected: Vec<_> = futures.collect();
+
+    // Wait for all spawned tasks to complete
+    let results = join_all(futures_collected).await;
+
+    // Check results
+    results
+        .into_iter()
+        .flatten()
+        .collect::<anyhow::Result<Vec<_>>>()?;
+
+    Ok(())
+}
+
